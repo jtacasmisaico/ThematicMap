8e0dd28c76a8ef3c143500decc2eb0cd4cb8a88e
Iain Dillingham <iain@dillingham.me.uk>
2014-04-06 11:09:26 +0100

    Deleted deprecated classes and interfaces

--- src/uk/me/dillingham/thematicmap/components/Drawable.java
+++
@@ -1,9 +0,0 @@
-package uk.me.dillingham.thematicmap.components;
-
-import processing.core.PApplet;
-import uk.me.dillingham.thematicmap.projections.Projection;
-
-public interface Drawable
-{
-    public void draw(PApplet p, Projection projection, float x, float y);
-}
