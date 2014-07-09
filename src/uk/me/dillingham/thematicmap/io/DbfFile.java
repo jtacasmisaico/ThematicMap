1a9428fcebd142e7fb546996f3dceea7442f753c
Iain Dillingham <iain@dillingham.me.uk>
2014-07-09 19:05:25 +0100

    Read field descriptors

--- src/uk/me/dillingham/thematicmap/io/DbfFile.java
+++ src/uk/me/dillingham/thematicmap/io/DbfFile.java
@@ -4,21 +4,27 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.nio.charset.Charset;
+import java.util.Arrays;
 
 import processing.core.PApplet;
 
 public class DbfFile
 {
+    private Charset charset;
+
     public DbfFile()
     {
-
+        charset = Charset.forName("US-ASCII");
     }
 
     public void read(InputStream inputStream) throws IOException
     {
+        // http://ulisse.elettra.trieste.it/services/doc/dbase/DBFstruct.htm#C1
+
         // File header
 
-        ByteBuffer fileHeader = ByteBuffer.allocate(67);
+        ByteBuffer fileHeader = ByteBuffer.allocate(32);
 
         inputStream.read(fileHeader.array());
 
@@ -26,9 +32,26 @@ public class DbfFile
 
         int numRecords = fileHeader.getInt(4);
 
-        short numBytesHeader = fileHeader.getShort(8);
+        short numHeaderBytes = fileHeader.getShort(8);
 
-        short numBytesRecord = fileHeader.getShort(10);
+        short numRecordBytes = fileHeader.getShort(10);
+
+        // Field descriptors
+
+        for (int currentHeaderByte = 32; currentHeaderByte < numHeaderBytes - 1; currentHeaderByte += 32)
+        {
+            ByteBuffer fieldDescriptor = ByteBuffer.allocate(32);
+
+            inputStream.read(fieldDescriptor.array());
+
+            fieldDescriptor.order(ByteOrder.LITTLE_ENDIAN);
+
+            String fieldName = new String(Arrays.copyOfRange(fieldDescriptor.array(), 0, 10), charset);
+
+            char fieldType = fieldDescriptor.getChar(11);
+
+            System.out.println(fieldType + " " + fieldName);
+        }
 
         inputStream.close();
     }
